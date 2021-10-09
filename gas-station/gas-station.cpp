3860cf01e4f8ebc31585f9ad6712a946b1d55bcf
BackInSchwarz <42225913+BackInSchwarz@users.noreply.github.com>
2021-10-09 10:55:15 -0400

    Time: 40 ms (32.80%), Space: 30.2 MB (17.15%) - LeetHub

---
+++ gas-station/gas-station.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+        int carry = 0;
+        pair<size_t, int> city_carry(0, 0);
+        for (size_t i = 1; i < gas.size(); ++i) {
+            carry += gas[i - 1] - cost[i - 1];
+            if (carry < city_carry.second) {
+                city_carry = {i, carry};
+            }
+        }
+        carry += gas[gas.size()-1] - cost[gas.size()-1];
+        return carry >= 0 ? city_carry.first : -1;
+    }
+};
