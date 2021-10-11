c74e1ac943880e87b0e8618ce3bdd1efe6947b8e
BackInSchwarz <42225913+BackInSchwarz@users.noreply.github.com>
2021-10-11 07:51:33 -0400

    Time: 99 ms (5.08%), Space: 20.8 MB (8.31%) - LeetHub

---
+++ word-break/word-break.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    map<int, bool> dp;
+    bool helper(unordered_set<string> dict, string s, int start){
+        if (start>(s.length()-1)) return true;
+        // general case. 
+        
+        if (dp.find(start)!=dp.end()) return dp[start];
+        
+        bool ans = false;
+        for (int i = start; i< s.length(); i++){
+            if (dict.find(s.substr(start, i-start+1))!= dict.end())
+                ans = ans || helper(dict, s, start + (i-start+1));
+        }
+        
+        dp[start] = ans;
+        
+        return ans;
+    }
+    
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+        return helper(dict, s, 0);
+    }
+};
+
+
+//recursion without actual answer. just a bool 
+//variables. 
+    //Source, 
+    //start: till which index, it is processed
+//logic
+    //end condition
+    //initial condition
+    //general condition.
+//memorization
+    //
