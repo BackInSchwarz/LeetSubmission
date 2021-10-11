0a0604fd03e4ea87411b2f1310215d87a277d28c
BackInSchwarz <42225913+BackInSchwarz@users.noreply.github.com>
2021-10-11 09:28:47 -0400

    Time: 13 ms (36.67%), Space: 8 MB (58.72%) - LeetHub

--- linked-list-cycle/linked-list-cycle.cpp
+++ linked-list-cycle/linked-list-cycle.cpp
@@ -9,20 +9,32 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if (head == NULL) return false;
-        // if (head->next == NULL) return false;
-        map<ListNode*, bool> ht;
-        ht[head] = true;
-        while ((head->next)!=NULL){
-            // check if we can move on
-            if (ht.find(head->next)!=ht.end())
+//         if (head == NULL) return false;
+//         // if (head->next == NULL) return false;
+//         map<ListNode*, bool> ht;
+//         ht[head] = true;
+//         while ((head->next)!=NULL){
+//             // check if we can move on
+//             if (ht.find(head->next)!=ht.end())
+//                 return true;
+//             // move on
+//             ht[head->next] = true;
+//             head = head->next;
+//         }
+        
+//         return false;
+        if (head==NULL) return false;
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while (slow!=NULL && fast != NULL && fast->next!=NULL){
+            if (slow == fast)
                 return true;
-            // move on
-            ht[head->next] = true;
-            head = head->next;
+            slow = slow->next;
+            fast = fast->next->next;
         }
         
         return false;
+        
     }
 };
 
