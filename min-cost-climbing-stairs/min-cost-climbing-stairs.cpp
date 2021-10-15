b0d44be0eeff191c849d234d17b08d6f5993f589
BackInSchwarz <42225913+BackInSchwarz@users.noreply.github.com>
2021-10-15 08:43:42 -0400

    Time: 7 ms (58.24%), Space: 13.8 MB (44.08%) - LeetHub

--- min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -7,17 +7,17 @@ public:
         
         // dp[0] = 0;
         dp[0] = cost[0];
-        cout<<dp[0]<<endl;
+        // cout<<dp[0]<<endl;
         if (n==1) return dp[n-1];
         
         
         dp[1] = min(dp[0]+ cost[1], cost[1]);
-        cout<<dp[1]<<endl;
+        // cout<<dp[1]<<endl;
         if (n==2) return min(dp[0], dp[1]);
         
         for (int i =2; i<n;i++){
             dp[i] = min(dp[i-1] + cost[i], dp[i-2] + cost[i]);
-            cout<<dp[i]<<endl;
+            // cout<<dp[i]<<endl;
         }
         dp[n] = min(dp[n-1], dp[n-2]);
         
