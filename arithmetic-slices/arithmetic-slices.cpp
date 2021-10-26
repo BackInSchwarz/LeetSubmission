b1b539463ab7bc08e77227ca2330ae310720a8f7
BackInSchwarz <42225913+BackInSchwarz@users.noreply.github.com>
2021-10-26 11:34:50 -0400

    Time: 4 ms (53.14%), Space: 7.5 MB (15.85%) - LeetHub

---
+++ arithmetic-slices/arithmetic-slices.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        vector<int> dp(nums.size(),0);
+        int sum = 0;
+        for (int i = 2; i<nums.size();i++){
+            if ((nums[i] - nums[i-1]) == (nums[i-1] - nums[i-2])){
+                dp[i] = 1+ dp[i-1];
+                sum+=dp[i];
+            }
+        }
+        
+        return sum;
+    }
+};
