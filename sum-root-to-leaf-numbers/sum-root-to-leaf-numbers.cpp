c52c3327f24bcde7ed3e480f6236bdd6f5daf5ba
BackInSchwarz <42225913+BackInSchwarz@users.noreply.github.com>
2021-11-03 14:03:25 -0400

    Time: 0 ms (100.00%), Space: 9 MB (98.25%) - LeetHub

--- sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
+++ sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
@@ -11,58 +11,22 @@
  */
 class Solution {
 public:
-    vector<vector<int>> helper(TreeNode* root){
-        vector<vector<int>> ans;
-        if (root==NULL) return ans;
-        
-        
-        vector<vector<int>> l_ans = helper(root->left);
-        vector<vector<int>> r_ans = helper(root->right);
-        
-        if (l_ans.size()==0 && r_ans.size()==0){
-            ans.push_back({root->val});
-            return ans;
+    int ans;
+    
+    void helper(TreeNode* root, int lastLevel){
+        if (root == NULL) return;
+        int curSum = lastLevel*10+ root->val;
+        if (root->left==NULL && root->right==NULL){
+            ans+=curSum;
+        }else{
+            if (root->left != NULL) helper(root->left, curSum);
+            if (root->right !=NULL) helper(root->right, curSum);
         }
-        
-        if (r_ans.size()==0){
-            for (int i = 0; i<l_ans.size();i++){
-                l_ans[i].push_back(root->val);
-            }
-            return l_ans;
-        }
-        
-        if (l_ans.size()==0){
-            for (int i = 0; i<r_ans.size();i++){
-                r_ans[i].push_back(root->val);
-            }
-            return r_ans;
-        }
-        
-        for (int i = 0; i<l_ans.size();i++){
-            l_ans[i].push_back(root->val);
-        }
-        
-        for (int i = 0; i<r_ans.size();i++){
-            r_ans[i].push_back(root->val);
-            l_ans.push_back(r_ans[i]);
-        }
-        
-        return l_ans;
+        return;
     }
     int sumNumbers(TreeNode* root) {
-        vector<vector<int>> ans = helper(root);
-        long int sum=0;
-        long mult = 1;
-        for (int i =0; i <10;i++){
-            for (int j=0;j<ans.size();j++){
-                if (i<ans[j].size()){
-                    cout<<ans[j][i]*mult<<endl;
-                    sum += ans[j][i]*mult;
-                }
-            }
-            mult = mult*10;
-        }
-        
-        return sum;
+        ans = 0;
+        helper(root, 0);
+        return ans;
     }
 };
