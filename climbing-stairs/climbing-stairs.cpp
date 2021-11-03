1627723e4f94ef43ba43685d01c28eb2e80c994f
BackInSchwarz <42225913+BackInSchwarz@users.noreply.github.com>
2021-11-03 17:29:56 -0400

    Time: 0 ms (100.00%), Space: 6.1 MB (26.35%) - LeetHub

--- climbing-stairs/climbing-stairs.cpp
+++ climbing-stairs/climbing-stairs.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
-    map<int, int> dp;
     int climbStairs(int n) {
-        if (n == 1) return 1;
-        if (n == 2) return 2;
-        int ans;
-        if (dp.find(n)!=dp.end()) return dp[n];
-        ans = climbStairs(n-1) + climbStairs(n-2);
-        dp[n] = ans;
-        return ans;
+        vector<int> dp(n+1,0);
+        dp[0] = 1;
+        dp[1] = 1;
+        // dp[2] = 2;
+        for (int i = 2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        return dp[n];
     }
 };
