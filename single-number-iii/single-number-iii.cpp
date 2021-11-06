39b343b7f8f386b6d19ad0d61fbdbcdf2129d852
BackInSchwarz <42225913+BackInSchwarz@users.noreply.github.com>
2021-11-06 07:55:29 -0400

    Time: 12 ms (42.79%), Space: 10 MB (76.63%) - LeetHub

--- single-number-iii/single-number-iii.cpp
+++ single-number-iii/single-number-iii.cpp
@@ -1,39 +1,18 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        int ab = nums[0];
-        for (int i = 1; i<nums.size();i++){
-            ab = ab^nums[i];
+        long xy = 0;
+        for (int i = 0;i<nums.size();i++){
+            xy = xy^nums[i];
         }
-        int diffbitN = 0;
-        for (int i = 0;i <32;i++){
-            if (((ab>>i)&1)==1){
-                diffbitN=i;
-                break;
-            }
+        
+        long diff = xy&(-xy);
+        
+        int x = 0;
+        for (int i = 0;i<nums.size();i++){
+            if ((nums[i]&diff)!=0) x^=nums[i];
         }
-        int a;
-        int a_set = 0;
-        int b;
-        int b_set = 0;
-        for (int i = 0; i<nums.size();i++){
-            if (((nums[i]>>diffbitN)&1)==1){
-                if (a_set==0){
-                    a = nums[i];
-                    a_set=1;
-                }else{
-                    a = a^nums[i];
-                }
-            }else{
-                if (b_set==0){
-                    b = nums[i];
-                    b_set=1;
-                }else{
-                    b = b^nums[i];
-                }
-                
-            }
-        }
-        return {a, b};
+        
+        return {x, int(xy^x)};
     }
 };
