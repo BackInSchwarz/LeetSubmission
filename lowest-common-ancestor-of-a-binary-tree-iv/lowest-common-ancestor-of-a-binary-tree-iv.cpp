20ebaf277f2d1afcb095db64ae62ff6654bc4698
BackInSchwarz <42225913+BackInSchwarz@users.noreply.github.com>
2021-12-08 19:27:25 -0500

    Time: 136 ms (16.71%), Space: 46.7 MB (13.07%) - LeetHub

---
+++ lowest-common-ancestor-of-a-binary-tree-iv/lowest-common-ancestor-of-a-binary-tree-iv.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int helper(TreeNode* root, set<TreeNode*> &nodes_set, TreeNode* & ans){
+        //NULL check
+        if(!root) return 0;
+        //self check
+        int cnt_root =0;
+        if(nodes_set.find(root)!=nodes_set.end()){
+            cnt_root++;
+        }
+        //children check
+        int left = helper(root->left, nodes_set, ans);
+        int right = helper(root->right, nodes_set, ans);
+        
+        //find answer
+        if(cnt_root==0){
+            //case 3
+            if(left>0&&right>0&&((left+right)==nodes_set.size())){
+                ans = root;
+            }
+        }else{
+            //cnt_root>0
+            //case 4
+            if(left>0&&right>0&&((left+right+cnt_root)==nodes_set.size())){
+                ans = root;
+            }
+            // case 1
+            if(left>0&&(right==0)&&(left+cnt_root)==nodes_set.size()) ans = root;
+            //case 2
+            if(left==0&&right>0&&(right+cnt_root)==nodes_set.size()) ans = root;
+               
+        }
+        
+        
+        // build answer
+        return cnt_root+left+right;
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, vector<TreeNode*> &nodes) {
+        //case 1: left + root and right ==0
+        //case 2: right + root and left ==0
+        //case 3: left + right and root==0
+        //case 4: left + root + right. and they all greater>0
+        if(nodes.size()==1) return nodes[0];
+        
+        TreeNode* ans;
+        set<TreeNode*> nodes_set(nodes.begin(), nodes.end());
+        helper(root, nodes_set, ans);
+        
+        return ans;
+    }
+};
