874377b29fe239896c9bcb661a70df945a7a603f
BackInSchwarz <42225913+BackInSchwarz@users.noreply.github.com>
2021-12-17 08:32:10 -0500

    Time: 44 ms (16.18%), Space: 11.2 MB (85.37%) - LeetHub

--- maximal-square/maximal-square.cpp
+++ maximal-square/maximal-square.cpp
@@ -1,64 +1,43 @@
 class Solution {
 public:
-    int getSum(vector<vector<int>>& matrix, int i,int j){
-        if (i<0||j<0) return 0;
-        
-        return matrix[i][j];
-    }
     int maximalSquare(vector<vector<char>>& matrix) {
-        //find pre fix sum
-        
-
-        
         int r = matrix.size();
         int c = matrix[0].size();
         
-        vector<vector<int>> dp(r, vector<int>(c,0));
+        int curMax = 0;
         
-        dp[0][0] = matrix[0][0]-'0';
-        
-        for (int i = 1;i<r;i++){
-            int temp = dp[i-1][0] + (matrix[i][0]-'0');
-            // cout<<temp<<endl;
-            dp[i][0]=temp;
-        }
-        
-        for (int i = 1;i<c;i++){
-            int temp = dp[0][i-1] + (matrix[0][i] - '0');
-            dp[0][i]=temp;
-        }
-        
-        for (int i=1;i<r;i++){
-            for (int j = 1;j<c;j++){
-                int temp = (matrix[i][j]-'0');
-                dp[i][j]  = temp +  dp[i-1][j]+dp[i][j-1] - dp[i-1][j-1];
-            }
-        }
-        // for (int i =0;i<r;i++){
-        //     for (int j = 0;j<c;j++){
-        //         cout<<dp[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        //try every possible square. 
-            // n>1 case
-        int n = min(r,c);
-        
-        for (int k = n;k>1;k--){
-            for(int i = k-1;i<r;i++){
-                for (int j=k-1;j<c;j++){
-                    int totalsum = getSum(dp, i,j) +getSum(dp, i-k,j-k) - getSum(dp, i-k,j) - getSum(dp, i,j-k);
-                    
-                    // cout<<k<<" "<<i<<" "<<j<<" "<<totalsum<<endl;
-                    if (totalsum ==(k*k)) return k*k; 
-                }
+        for (int i = 0; i<r; i++){
+            if (matrix[i][0]=='1'){
+                curMax = 1;
+                break;
             }
         }
         
-            // n=1 case check last in the matrix
-        if (dp[r-1][c-1]>0) return 1;
+        for (int j = 0; j<c; j++){
+            if (matrix[0][j]=='1'){
+                curMax = 1;
+                break;
+            }
+        }
         
         
-        return 0;
+        for (int i = 1; i<r ;i++){
+            for (int j = 1; j < c; j++){
+                if (matrix[i][j]=='0') continue;
+                
+                int topL = matrix[i-1][j-1] - '0';
+                int l = matrix[i-0][j-1] - '0';
+                int r = matrix[i-1][j-0] - '0';
+                
+                int ans = min( l, min( topL, r) ) + 1;
+                
+                if ((ans*ans) > curMax) curMax = (ans*ans);
+                
+                matrix[i][j] = (ans + '0');
+            }
+        }
+        
+        
+        return curMax;
     }
 };
