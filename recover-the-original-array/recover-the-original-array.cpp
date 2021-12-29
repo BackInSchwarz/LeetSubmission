16c1793a4d413a14c58c0248fd888652241b1699
BackInSchwarz <42225913+BackInSchwarz@users.noreply.github.com>
2021-12-29 14:50:25 -0500

    Time: 1128 ms (62.33%), Space: 417.4 MB (25.91%) - LeetHub

--- recover-the-original-array/recover-the-original-array.cpp
+++ recover-the-original-array/recover-the-original-array.cpp
@@ -1,41 +1,62 @@
 class Solution {
 public:
-    vector<int> recoverArray(vector<int>& nums) {  
-        sort(nums.begin(), nums.end());
-        int n2 = nums.size();
-        int n = n2/2;
-        int a = nums[0];
-        vector<int> ans(n,0);
-      
-        for (int i = 1; i <= n; i++)
-        {
-            int k2 = nums[i] - a;
-            if (k2 % 2 == 1 || k2 == 0 || nums[i] == nums[i - 1]) continue;   
-            int k = k2/2;
-            int i1 = 1;
-            int i2 = 0;
-            ans[0] = a + k;
-            for (int j = 1; j < n2; j++)
-            {
-                int nj = nums[j];
-                if (i2 < i1 && (nj == ans[i2] + k)) {
-                    //append nj to higher
-                    i2++;
-                } else if (i1 < n) {
-                    // append nj to lower 
-                    // update ans
-                    ans[i1++] = nj + k;
-                } else {
-                    break;
-                }
-            }
+vector<int> recoverArray(vector<int>& nums) {
         
-            if (i1 == n && i2 == n)
-                break;
-
+		//sorting the nums array
+        sort(nums.begin(),nums.end());
+        
+		//we'll try every nums[i] as first element of higher array excpet nums[0]
+		//as it is first element of lower
+        for(int i=1;i<size(nums);i++)
+        {
+            vector<int> a;
+			//pushing minimum element in lowest
+            a.push_back(nums[0]);
+			
+			//calculating k after assuming nums[i] as first element of higher
+            int k = nums[i] - a.back();
+            
+			//in case k is odd or k = 0, skip !
+            if(k&1 || k == 0) continue;
+            
+            multiset<int> st(nums.begin(),nums.end());
+			
+			//delete used elements ,ie, nums[0] and nums[i]
+            st.erase(st.find(a.back())) , st.erase(st.find(nums[i]));
+			
+            while(!st.empty())
+            {
+                // now current minimum value will be part of lower array, so pushing it to a
+				// and deleting from multiset
+                a.push_back(*st.begin());
+                st.erase(st.begin());
+				
+				//now corresponding element in higher should be
+				// last pushed element in lower + current k
+                auto it = st.find(a.back() + k);
+				
+				//if we cann't find corresponding element in higher, we'll break the loop
+                if(it == st.end()) break;
+				
+				//else delete it from multiset
+                st.erase(it);
+            }
+            
+			//now if our multiset is empty, ie , we have used all the elements then
+			// it is clear that current value of k is the right choice
+			// increment every value of lower by k/2 as higher[i] - lower[i] = 2*k
+			// and we need to add k to lower[i] to make current array
+            if(st.empty())
+            {
+                for(auto &val : a) val += k/2;
+                // cout<<"\n";
+                return a;
+            }
+            
         }
-      
-        return ans; 
+        
+        assert(false);
+        
     }
 };
 
